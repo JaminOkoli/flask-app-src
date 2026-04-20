307c17e1755dd9609906c7f3f40f33d959242c4d
JaminOkoli <jaminokoli10@gmail.com>
2026-04-19 23:26:57 -0400

    Fix Pipeline

--- test_app.py
+++ test_app.py
@@ -4,6 +4,5 @@ def test_health():
     client = app.test_client()
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json["status"] == "healthy"
-   # assert response.json["status"] == "ok"
+    assert response.json["status"] == "ok"
    
